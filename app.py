98702f1651745c4f6d865f95c2d32d6f50c98253
Batulat <taofeekayoolabatula@gmail.com>
2024-10-10 10:43:25 +0100

    Completed arithemetic operation of all function
    Added subFunction
    Added Divfunction
    Added prodFunction
    Added additionFunct

--- app.py
+++ app.py
@@ -2,4 +2,11 @@ def department():
     name = input('What is your name?')
     print(name)
 
-department()
+def addFunction(x,y):
+    return x + y
+def subFunction(x,y):
+    return x - y
+def divFunction(x,y):
+    return x / y
+def multiplyFunction(x,y):
+    return x * y
